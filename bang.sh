1f2349220a757fb21a3437204be83e65165e8494
hasanteagyeman <henryasanteagyeman@gmail.com>
2025-02-01 13:19:34 -0500

    my second bang

--- bang.sh
+++ bang.sh
@@ -13,3 +13,13 @@ PING github.com (140.82.113.3) 56(84) bytes of data.
 --- github.com ping statistics ---
 10 packets transmitted, 10 received, 0% packet loss, time 9018ms
 rtt min/avg/max/mdev = 16.258/21.638/27.119/3.583 ms
+kingdavid:x:1002:1002::/home/kingdavid:/bin/bash
+harry:x:1003:1004::/home/harry:/bin/bash
+natasha:x:1004:1005::/home/natasha:/bin/bash
+sarah:x:1005:1006::/home/sarah:/sbin/nologin
+nginx:x:973:973:Nginx web server:/var/lib/nginx:/sbin/nologin
+nagios:x:1006:1007::/home/nagios:/bin/bash
+paul:x:1007:1009::/home/paul:/bin/bash
+mysql:x:27:27:MySQL Server:/var/lib/mysql:/sbin/nologin
+babubutt:x:1008:1010::/home/babubutt:/bin/bash
+devops:x:1009:1011::/home/devops:/bin/bash
